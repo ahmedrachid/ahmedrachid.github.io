8afee1fd75790aaad8fd195f5af45ae02302ce7a
Ahmed Hazourli <ahmed.hazourli@u-psud.fr>
2020-06-21 01:06:34 +0200

    Updates

---
+++ precache-manifest.e05b51d19477785d1b42aefe5e6ffc4b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56fd10537ec854d754057abd4b335dbe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f8efa5fe9648d8fb300",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "78ee0ed2e29931f8f9a3",
+    "url": "/static/js/0.a34b4197.chunk.js"
+  },
+  {
+    "revision": "9e4a026045191cb6ac40",
+    "url": "/static/js/1.a1843281.chunk.js"
+  },
+  {
+    "revision": "354ae6f9f1bc5e462d60",
+    "url": "/static/js/4.0b024a90.chunk.js"
+  },
+  {
+    "revision": "2612de63c4e44a1b46dc389fd51cd6bb",
+    "url": "/static/js/4.0b024a90.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2f8efa5fe9648d8fb300",
+    "url": "/static/js/main.dfd2a944.chunk.js"
+  },
+  {
+    "revision": "dd35b3039b1566a63865",
+    "url": "/static/js/runtime-main.bbd5b0dc.js"
+  }
+]);
